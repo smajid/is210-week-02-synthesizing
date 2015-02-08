3f58108efbcd3ecf98affa96e755bed177e23ab4
Saulat Majid <saulat.majid@spsmail.cuny.edu>
2015-02-08 18:35:27 +0000

    Tests conducted, problems fixed.

--- task_03.py
+++ task_03.py
@@ -1,7 +1,9 @@
-CHARLIE = "Chocolate Factory" 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This is Charlie the Chocolate Factory"""
+
+
+CHARLIE = "Chocolate Factory"
 VIOLET = "Gray"
 PATRICIA = "Reichardt"
 LINUS = "van Pelt"
-
-
-
